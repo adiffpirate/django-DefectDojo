12bee9ca9982be07dbed260297ea53db2d17385e
adiffpirate <lpsmonteiro@gmail.com>
2021-05-14 19:15:52 -0300

    Minor fix

--- dojo/tools/sarif/parser.py
+++ dojo/tools/sarif/parser.py
@@ -141,7 +141,7 @@ def get_item(result, rules, artifacts, run_date):
             file_path = location['physicalLocation']['artifactLocation']['uri']
             # 'region' attribute is optionnal
             if 'region' in location['physicalLocation']:
-                code_snippet = location['physicalLocaltion']['region']['snippet']['text']
+                code_snippet = location['physicalLocation']['region']['snippet']['text']
                 line = location['physicalLocation']['region']['startLine']
 
     # test rule link
